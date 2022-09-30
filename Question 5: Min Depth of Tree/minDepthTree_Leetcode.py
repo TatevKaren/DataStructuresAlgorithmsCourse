3107b6f25dafc3b69d90cbe5fea7a6d9eb65380b
TK_Aslanyan <76843403+TatevKaren@users.noreply.github.com>
2022-09-30 05:23:18 -0700

    Add files via upload

---
+++ Question 5: Min Depth of Tree/minDepthTree_Leetcode.py
@@ -0,0 +1,11 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        return (1+ min(self.minDepth(root.left), self.minDepth(root.right)) )
